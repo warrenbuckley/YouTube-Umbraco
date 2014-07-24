74b20fd0dc5ab38e56ea4467c04740f3b55838b9
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-07-24 19:31:05 +0100

    Adding all command to MyGet.bat

--- BuildPackage/MyGet.bat
+++ BuildPackage/MyGet.bat
@@ -1,1 +1,6 @@
-Package.build.cmd
+cd ..\YouTube.PropertyEditors\YouTube\
+Call npm install
+Call grunt
+cd ..\..\BuildPackage\
+Call C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
+pause
