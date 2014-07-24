2f43aae7d77c03f321eed62c51d1d2ae2153ddc8
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-07-24 21:25:47 +0100

    And again

--- BuildPackage/MyGet.bat
+++ BuildPackage/MyGet.bat
@@ -1,6 +1,6 @@
 cd YouTube.PropertyEditors\YouTube\
 Call npm install
 Call node -e "require('grunt').tasks(['default']);"
-Call ..\..\.nuget\nuget.exe restore ..\YouTube.sln
+Call ..\..\.nuget\nuget.exe restore ..\..\YouTube.sln
 cd ..\..\BuildPackage\
 Call C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
