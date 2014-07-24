7eaec7ed781fdeb0b7d43780f6eb56bffabf22ce
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-07-24 19:42:44 +0100

    One more path tweak

--- BuildPackage/MyGet.bat
+++ BuildPackage/MyGet.bat
@@ -1,6 +1,6 @@
 cd YouTube.PropertyEditors\YouTube\
 Call npm install
 Call grunt
-cd BuildPackage\
+cd ..\..\BuildPackage\
 Call C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
 pause
