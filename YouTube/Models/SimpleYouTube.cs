b0ad42305ef2e288b07cc4a6facc7b17e17d9215
Stefano Chiodino <chiodinostefano@gmail.com>
2016-11-04 21:58:06 +0000

    Change YouTube to SimpleYouTube that contains only the elements in the DB.

---
+++ YouTube/Models/SimpleYouTube.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace YouTube.Models
+{
+    /// <summary>
+    /// The YouTube return model
+    /// </summary>
+    public class SimpleYouTube : IEnumerable<SelectedVideo>
+    {
+        /// <summary>
+        /// The selected videos.
+        /// </summary>
+        private readonly IEnumerable<SelectedVideo> _selectedVideos;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="YouTube"/> class.
+        /// </summary>
+        /// <param name="selectedVideos">
+        /// The selected videos.
+        /// </param>
+        public SimpleYouTube(List<SelectedVideo> selectedVideos)
+        {
+            _selectedVideos = selectedVideos.ToList() ?? new List<SelectedVideo>();
+        }
+
+        /// <summary>
+        /// The get enumerator.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerator"/>.
+        /// </returns>
+        public IEnumerator<SelectedVideo> GetEnumerator()
+        {
+            return this._selectedVideos.GetEnumerator();
+        }
+
+        /// <summary>
+        /// The get enumerator.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerator"/>.
+        /// </returns>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ YouTube/SelectedVideoListValueConverter.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web;
+using YouTube.Models;
+
+namespace YouTube
+{
+    /// <summary>
+    /// The List of SelectedVideo property value converter.
+    /// </summary>
+    [PropertyValueType(typeof(IEnumerable<global::YouTube.Models.SelectedVideo>))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+    public class SelectedVideoListValueConverter : IPropertyValueConverter
+    {
+        /// <summary>
+        /// Checks if this converter can convert the property editor and registers if it can.
+        /// </summary>
+        /// <param name="propertyType">
+        /// The published property type.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("YouTube.Channel");
+        }
+
+        /// <summary>
+        /// Convert the JSON data into a array
+        /// </summary>
+        /// <param name="propertyType">
+        /// The published property type.
+        /// </param>
+        /// <param name="source">
+        /// The value of the property
+        /// </param>
+        /// <param name="preview">
+        /// The preview.
+        /// </param>
+        /// <returns>
+        /// The <see cref="object"/>.
+        /// </returns>
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            if (source != null)
+            {
+                var selectedVideoList = JsonConvert.DeserializeObject<List<SelectedVideo>>(source.ToString());
+            }
+
+            return new string[0];
+        }
+
+        /// <summary>
+        /// Convert the source array into a Video object
+        /// </summary>
+        /// <param name="propertyType">
+        /// The published property type.
+        /// </param>
+        /// <param name="source">
+        /// The value of the property
+        /// </param>
+        /// <param name="preview">
+        /// The preview.
+        /// </param>
+        /// <returns>
+        /// The <see cref="object"/>.
+        /// </returns>
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+        /// <summary>
+        /// Convert the source array into a CSV string
+        /// </summary>
+        /// <param name="propertyType">
+        /// The published property type.
+        /// </param>
+        /// <param name="source">
+        /// The value of the property
+        /// </param>
+        /// <param name="preview">
+        /// The preview.
+        /// </param>
+        /// <returns>
+        /// The <see cref="object"/>.
+        /// </returns>
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            var selectedVideos = (string[])source;
+            return string.Join(",", selectedVideos);
+        }
+    }
+}
